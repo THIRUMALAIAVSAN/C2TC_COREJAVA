bc9ffcabf8de47d6359db41d84f3c3a83fb0b305
THIRUMALAIAVSAN <109678997+THIRUMALAIAVSAN@users.noreply.github.com>
2023-07-30 09:34:17 +0530

    Create Identifiers.java

---
+++ Identifiers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class identifiers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		System.out.println(x);
+		System.out.println("Hello World,It is Java Program!");
+		final int a=10;
+		System.out.println(a);
+		double j=13.5;
+		System.out.println(j);
+	}
+
+}
