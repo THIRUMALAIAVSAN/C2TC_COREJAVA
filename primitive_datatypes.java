81b079b7d3aef0bc96d4dc38d636d99337f851ef
THIRUMALAIAVSAN <109678997+THIRUMALAIAVSAN@users.noreply.github.com>
2023-07-30 10:14:30 +0530

    Create primitive_datatypes.java

---
+++ primitive_datatypes.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class primitive_datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		float y=sc.nextFloat();
+		char z=sc.next().charAt(0);
+		System.out.println("------------------------------------------");
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+
+	}
+
+}
