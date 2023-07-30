622dbe599c9ed8fb16ee3f9ad6203dcfabc103b0
THIRUMALAIAVSAN <109678997+THIRUMALAIAVSAN@users.noreply.github.com>
2023-07-30 10:13:30 +0530

    Create Type_casting.java

---
+++ Type_casting.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Type_Casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		double b=a;
+		System.out.println(a);
+		System.out.println(b);//print the statement of b
+		int c=10;
+		double d=(double) c;
+		System.out.println(d);
+}
+}
