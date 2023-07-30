80383d6750d87b2c41b1461362db38c3e499c04e
THIRUMALAIAVSAN <109678997+THIRUMALAIAVSAN@users.noreply.github.com>
2023-07-30 09:32:41 +0530

    Create character.java

---
+++ character.java
@@ -0,0 +1,18 @@
+
+public class Character {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] a= {'a','e','i','o','u'};
+		for(char b:a){
+			System.out.println(b);
+		}
+		System.out.println("----------------------------------------");
+		char[] b= {'A','E','I','O','U'};
+		for(char c:b) {
+			System.out.println(c);
+		}
+
+	}
+
+}
